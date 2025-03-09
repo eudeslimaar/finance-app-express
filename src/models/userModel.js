210ef4a35e91e6608a0272b55ded27ba377de69a
Eudes Lima <eudesfilho1994@outlook.com>
2025-03-09 19:25:42 -0300

    feat: melhorar estrutura do userRepository.js - Refatorado userRepository.js para usar o modelo User diretamente

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,13 +1,31 @@
 export default class User {
-    constructor(id, firstname, lastname, preferredName, email, username, password, createdAt) {
-      this.id = id;
-      this.firstname = firstname;
-      this.lastname = lastname;
-      this.preferredName = preferredName;
-      this.email = email;
-      this.username = username;
-      this.password = password;
-      this.createdAt = createdAt;
-    }
-  }
-  
+	constructor(id, firstname, lastname, preferredName, email, username, password, createdAt, updatedAt) {
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.preferredName = preferredName;
+		this.email = email;
+		this.username = username;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+		//private
+		this._password = password;
+	}
+
+	getPassword() {
+		return this._password;
+	}
+
+	toJSON() {
+		return {
+			id: this.id,
+			firstname: this.firstname,
+			lastname: this.lastname,
+			preferredName: this.preferredName,
+			email: this.email,
+			username: this.username,
+			createdAt: this.createdAt,
+			updatedAt: this.updatedAt
+		};
+	}
+}
--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -1,25 +1,33 @@
 import pool from "../config/database.js";
+import User from "../models/userModel.js";
 
 const getAllUsers = async () => {
-  const [rows] = await pool.query("SELECT * FROM users");
-  return rows;
+	const [rows] = await pool.query("SELECT * FROM users");
+	return rows.map(user => new User(...Object.values(user)));
 };
 
-const createUser = async (user) => {
-  const { firstname, lastname, preferredName, email, username, password } = user;
-  const [result] = await pool.query(
-    "INSERT INTO users (firstname, lastname, preferred_name, email, username, password) VALUES (?, ?, ?, ?, ?, ?)",
-    [firstname, lastname, preferredName, email, username, password]
-  );
-  return { id: result.insertId };
+const getUserById = async (id) => {
+	const [rows] = await pool.query("SELECT * FROM users WHERE id = ?", [id]);
+	return rows.length > 0 ? new User(...Object.values(rows[0])) : null;
 };
 
 const getUserByEmailOrUsername = async (email, username) => {
-  const [rows] = await pool.query(
-    "SELECT * FROM users WHERE email = ? OR username = ?",
-    [email, username]
-  );
-  return rows.length > 0 ? rows[0] : null;
+	const [rows] = await pool.query(
+		"SELECT * FROM users WHERE email = ? OR username = ?",
+		[email, username]
+	);
+	return rows.length > 0 ? new User(...Object.values(rows[0])) : null;
 };
 
-export default { getAllUsers, createUser, getUserByEmailOrUsername };
+const createUser = async (user) => {
+	const { firstname, lastname, preferredName, email, username, password } = user;
+
+	const [result] = await pool.query(
+		"INSERT INTO users (firstname, lastname, preferred_name, email, username, password) VALUES (?, ?, ?, ?, ?, ?)",
+		[firstname, lastname, preferredName, email, username, password]
+	);
+
+	return await getUserById(result.insertId);
+};
+
+export default { getAllUsers, getUserById, getUserByEmailOrUsername, createUser };
